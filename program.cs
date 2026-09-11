415bae45b3d4bd090e2bf0156c0d464186d733ec
S.Hegazy.inf24 <s.hegazy.inf24@stud.bbbaden.ch>
2026-09-11 10:04:35 +0200

    Dotnet correction

---
+++ program.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace TicTacToe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("<-------------------------------------->");
+            Console.WriteLine("Welcome in the Tic-Tac-Toe.");
+            Console.WriteLine("I hope you will like it.");
+            Console.WriteLine("<-------------------------------------->");
+            Console.WriteLine();
+            Console.WriteLine("Please type any taste for starting the game.");
+            Console.ReadKey();
+        }
+    }
+}
